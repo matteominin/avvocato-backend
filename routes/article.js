db145adfac75346bfc51886f8a860c68b30938ea
Matteo Minin <matteominin@yahoo.it>
2021-06-15 15:31:16 +0200

    authentication test

--- routes/article.js
+++ routes/article.js
@@ -16,15 +16,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/add', (req, res) => {
     const articolo = req.body
-    if(req.body.pass === process.env.pass){
+    /* if(req.body.pass === process.env.pass){ */
         req.body.date = new Date()
         let newArticle = new articoli(articolo)
         newArticle.save()
             .then(() => res.json('Articolo caricato'))
             .catch((err) => res.status(400).json('Error: ' + err))
-    }else{
+/*     }else{
         res.status(400).json('Autenticati per proseguire')
-    }
+    } */
 })
 
 module.exports = router
